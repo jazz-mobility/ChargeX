e22286cc506b51ac50a835cab7ace0fd4e565560
Jasveer Singh <jasveer.singh@deliveryhero.com>
2022-08-24 17:55:10 +0200

    Initial Commit

---
+++ ChargeX/ChargeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeXApp.swift
+//  ChargeX
+//
+//  Created by Jasveer Singh on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct ChargeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChargeX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChargeX
+//
+//  Created by Jasveer Singh on 24.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
